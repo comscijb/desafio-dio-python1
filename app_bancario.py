9af7d1f620783155a524ed6efc70232cb8745975
comscijb <comscijb@gmail.com>
2024-06-20 19:28:11 -0300

    Corrected some minor issues, added a try/catch block to verify the type of variable.

--- app_bancario.py
+++ app_bancario.py
@@ -16,36 +16,44 @@ while True:
     option = int(input(menu))
     
     if option == 1:
-        valor_deposito = float(input("Qual o valor do depósito? "))
-        
-        if valor_deposito <= 0:
-            print("Operação inválida. Por favor, insira um valor positivo.")
+        try:
+            valor_deposito = float(input("Qual o valor do depósito? "))
             
-        else:
-            saldo += valor_deposito
-            extrato.append(valor_deposito)
-            print(f"Depósito de R$ {valor_deposito:.2f} realizado com sucesso!")
+            if valor_deposito <= 0:
+                print("Operação inválida. Por favor, insira um valor positivo.")
+                
+            else:
+                saldo += valor_deposito
+                extrato.append(valor_deposito)
+                print(f"Depósito de R$ {valor_deposito:.2f} realizado com sucesso!")
+                
+        except ValueError:
+            print("Operação inválida. Por favor, insira um número positivo.")
     
     elif option == 2:
-        valor_saque = float(input("Qual o valor do saque? "))
-        
-        if saques_no_dia >= LIMITE_DE_SAQUES:
-            print("Limite de saques diários excedido. Operação cancelada!")
+        try:
+            valor_saque = float(input("Qual o valor do saque? "))
             
-        elif valor_saque > limite_por_saque:
-            print(f"Saque maior que o valor máximo permitido por saque. Valor maximo: R$ {limite_por_saque}")
-            
-        elif valor_saque > saldo:
-            print(f"Saldo insuficiente. Valor disponível: R$ {saldo}")
-            
-        elif valor_saque > 0:
-            saldo -= valor_saque
-            extrato.append(-valor_saque)
-            saques_no_dia += 1
-            print(f"Saque de R$ {valor_saque:.2f} realizado com sucesso!")
-            
-        else:
-            print("Operação inválida. O valor informado é inválido!")
+            if saques_no_dia >= LIMITE_DE_SAQUES:
+                print("Limite de saques diários excedido. Operação cancelada!")
+                
+            elif valor_saque > limite_por_saque:
+                print(f"Saque maior que o valor máximo permitido por saque. Valor maximo: R$ {limite_por_saque}")
+                
+            elif valor_saque > saldo:
+                print(f"Saldo insuficiente. Valor disponível: R$ {saldo}")
+                
+            elif valor_saque > 0:
+                saldo -= valor_saque
+                extrato.append(-valor_saque)
+                saques_no_dia += 1
+                print(f"Saque de R$ {valor_saque:.2f} realizado com sucesso!")
+                
+            else:
+                print("Operação inválida. O valor informado é inválido!")
+                
+        except ValueError:
+            print("Operação inválida. Por favor, insira um número.")
 
     elif option == 3:
         print("\nExtrato:\n")
@@ -58,9 +66,9 @@ while True:
                 if operacao > 0:
                     print(f"Depósito R$ +{operacao:.2f}")
                 else:
-                    print(f"Saque R$ {operacao:.2f}")
+                    print(f"Saque    R$ {operacao:.2f}")
                     
-        print(f"\nSaldo: R$ {saldo:.2f}\n")
+        print(f"\nSaldo:   R$ {saldo:.2f}\n")
 
     elif option == 0:
         print("Obrigado por utilizar nosso sistema!")
